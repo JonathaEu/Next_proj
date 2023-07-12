48c018d4f5e4f1b5b89bf8134d281de0ebe3cc11
Jonatha Cunha - TI <Jonatha.cunha@MASAN.COM.BR>
2023-07-12 15:00:51 -0300

    second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,22 @@ export default function home() {
   return (
     <div  className = {` 
     flex justify-center items-center  rounded-lg
-    border-2 m-10 ml-80 mr-80 bg-cyan-500  p-20
+    border-2 m-10 ml-80 mr-80 bg-cyan-500  
     font-verdana
     `}>
     <form action="" className = {` 
-     p-20  justify-between
+     p-20  justify-center items-center
     `}>
     <label htmlFor="nome">Nome:</label> <br />
     <input type="text" name="" id="nome" /> <br />
 
-    <label htmlFor="especie">Espécie:</label> <br />
-    <input type="text" name="" id="especie" /> <br />
+    <label htmlFor="cpf">CPF:</label> <br />
+    <input type="text" name="" id="cpf" /> <br />
 
-    <label htmlFor="peso">Peso:</label> <br />
-    <input type="text" name="" id="peso" /> <br />
-    
+    <label htmlFor="ano_nasc">Ano de nascimento:</label> <br />
+    <input type="text" name="" id="ano_nasc" /> <br />
+
+    <button type="submit" id="enviar" className={`rounded-md  flex  p-2`}>Enviar</button>    
     </form>
     </div>
   )
